eaf8b575167e00561e91a912ba1418bb380af311
mario <mario1995@live.it>
2018-09-03 15:06:11 +0200

    fixed server.js and improved instert style

---
+++ Server-EBSearch/paths-manager.js
@@ -0,0 +1,8 @@
+//THIS FILE CONTAINS ALL PATHS 
+
+//path to store all components
+var __projectsRepoPATH= __dirname+"/ProjectsRepo/";
+var __externalToolsPATH= __dirname+"/ExternalTools/";
+
+module.exports.projectsRepoPATH= __projectsRepoPATH;
+module.exports.externalToolsPATH= __externalToolsPATH;
