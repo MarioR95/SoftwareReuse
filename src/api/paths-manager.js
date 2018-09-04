61a99191f3a8255bdae076263539394870696e92
mario <mario1995@live.it>
2018-09-04 14:23:22 +0200

    fixed project structure

--- src/api/paths-manager.js
+++ src/api/paths-manager.js
@@ -1,8 +1,8 @@
 //THIS FILE CONTAINS ALL PATHS 
 
 //path to store all components
-var __projectsRepoPATH= __dirname+"/ProjectsRepo/";
-var __externalToolsPATH= __dirname+"/ExternalTools/";
+var __projectsRepoPATH= __dirname+"/../../repository/";
+var __externalToolsPATH= __dirname+"/../utility/";
 
 module.exports.projectsRepoPATH= __projectsRepoPATH;
 module.exports.externalToolsPATH= __externalToolsPATH;
