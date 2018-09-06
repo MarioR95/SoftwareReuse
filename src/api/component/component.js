8751a3a59bc0b6df9f14b2db1b61b1823ffcf694
mario <mario1995@live.it>
2018-09-06 11:39:47 +0200

    server code refcatoring

--- src/api/component/component.js
+++ src/api/component/component.js
@@ -8,10 +8,15 @@ var extract = require('extract-zip');
 //for run child process
 var exec = require('child_process').exec, child;
 var componentOperation = require('./component-operation');
+//for parse file JSON
+var GSON= require('gson');
+//for find file on directory
+var find = require('find');
+var sync = require('sync');
+
 
 module.exports.loadComponent = function (response,fields, files) {
     console.log("-Load Component "+files.filetoupload.name);
-    console.log(files);
     var currentPath = files.filetoupload.path;
     if(currentPath != undefined) {
         newPath =  paths.projectsRepoPATH+files.filetoupload.name;
@@ -19,46 +24,89 @@ module.exports.loadComponent = function (response,fields, files) {
         if (err)
             errorPage("Component not saved into repository");
         });
-        //start chain of function unZip() -> parseComponent() -> handleJSONFIle() -> insertComponent() -> (CONTINUE) handleJSONFIle() (because you have to do them synchronously)
-        console.log("-File unzipped correctly");
-        unZip(response,files,fields);
+        var source = newPath;
+        var target = paths.projectsRepoPATH;
+        //EXTRACT ZIP
+        extract(source, {dir: target}, function (err) {
+          if(err)
+            console.log("Errore"+err);
+          console.log("-File unzipped correctly");
+          //PARSE FILE INTO FOLDER 
+          if(fields.java != undefined){
+          	console.log("@@@@@@@@@HO INSERITO UN PROJETTO JAVA");
+          	parseJavaComponent(response,files,fields);
+            console.log("-Component parsed correctly");
+          }else 
+          	console.log("Language not supported");
+        });
+		
     }else 
         errorPage("-File format is not valid!");  
 }
 
-function unZip(response,files,fields) {
-    var source = newPath;
-    var target = paths.projectsRepoPATH;
-    extract(source, {dir: target}, function (err) {
-      if(err != undefined)
-        console.log(err);
-    });
-    console.log("-Unzipped successfully");
-    console.log("-Component parsed correctly");
-    if(fields.java != undefined){
-    	parseJavaComponent(response,files,fields);
-    }else 
-    	console.log("Language not supported");
-}
+
   
 function parseJavaComponent(response,files,fields) {
-//remove extention path
-newPath = newPath.split('.').slice(0, -1).join('.');
-console.log(paths.externalToolsPATH);
-child = exec('java -jar '+paths.externalToolsPATH+'JavaT.jar -path '+newPath+' -out '+paths.projectsRepoPATH,
-function (error, stdout, stderr){
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
-    if(error){
-    errorPage("-Parse Failed", response);
-    }else {
-    console.log("-Parsed correctly");
-    console.log("-Start handle JSon file");
-    componentOperation.doSave(response,files,fields);
-    }
-});
-
+    var contents;
+    //remove extention path
+    newPath = newPath.split('.').slice(0, -1).join('.');
+    console.log(paths.externalToolsPATH);
+    componentOperation.createProjectNode(files,fields);
+    beforeStoring(fields);
+    componentOperation.endOperations(response);
+    
 }
+
+
+function beforeStoring(fields){
+   	if(fields.source != undefined) {
+   		var cls=[], dependencies=[];
+        child = exec('java -jar '+paths.externalToolsPATH+'JavaT.jar -path '+newPath+' -out '+paths.projectsRepoPATH,
+        function (error, stdout, stderr){
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            if(error){
+            	errorPage("-Error during file parsing into exec", response);
+            }else {
+                console.log("-File parsed correctly");
+                console.log("-Start handle JSon file...");
+                contents = fs.readFileSync(paths.projectsRepoPATH+"result.json");
+                var jsonContent = [];
+                jsonContent = GSON.parse(contents);
+                for(i = 0; i < Object.keys(jsonContent.class).length; i++) {
+                    cls[i] = jsonContent.class[i];
+                    for(j = 0; j < Object.keys(jsonContent.dependencies).length;j++) {
+                        dependencies[j] = jsonContent.dependencies[j];
+                    }  
+                }
+                console.log("JSon file parsed correctly");
+                fs.unlink(paths.projectsRepoPATH+'result.json');
+                componentOperation.doSaveSourceFile(cls,dependencies);
+            }
+        });
+    }
+    if (fields.doc != undefined) {
+	   var documents = [];
+	   //search any file for documentation on project
+       console.log("PATH"+newPath);
+       find.file(/([a-zA-Z0-9\s_\\.\-\(\):])+(.doc|.docx|.pdf|.html|.htm|.odt|.xls|.xlsx|.ods|.ppt|.pptx|.txt)$/i ,newPath, function(documents) {
+           console.log("NEWPATH: "+newPath);
+           console.log("DOCUMENTATION: "+documents);
+           componentOperation.doSaveDocuments(documents);
+       });
+     
+        
+    }
+    if(fields.test != undefined) {
+        find.file(/^.*test.*$/,newPath,function (tests) {
+            console.log("exist directory test");
+            console.log("Test files "+tests);
+            componentOperation.doSaveTestFile(tests);
+        });
+    }
+}
+
+
 function errorPage(mess) {
     fs.unlinkSync(completePath);
     console.log("**********REMOVING COMPONENT FROM REPOSITORY AFTER ERROR: "+mess+"**********");
